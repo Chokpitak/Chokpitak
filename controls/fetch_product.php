1f1605fada0afd41123db9462f4c9151681b1dc4
Chokpitak <muayio538@gmail.com>
2025-07-17 01:12:42 +0700

    Update fetch_product.php

--- controls/fetch_product.php
+++ controls/fetch_product.php
@@ -2,7 +2,7 @@
     include './controls/db2.php';
     session_start();
 
-    $sql = "SELECT `id`, `product_name`, `description`, `price`, `created_at` FROM `products`";
+    $sql = "SELECT `id`, `product_name`, `description`, `price`, `created_at`, `profile_image` FROM `products`";
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
 ?>
